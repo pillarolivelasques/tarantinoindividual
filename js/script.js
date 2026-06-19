4a48e6f3af57f814f83a7e69df7935231329d578
pillarolivelasques <pillarolivelasques@gmail.com>
2026-06-19 11:10:42 +0200

    fix: pass this to selectTicket to fix card selection bug

--- js/script.js
+++ js/script.js
@@ -173,16 +173,3 @@ function checkout() {
 }
 
 
-/* 
-   LOADER
- */
-window.addEventListener('load', function() {
-  var loader = document.getElementById('loader');
-  if (!loader) return;
-  setTimeout(function() {
-    loader.style.opacity = '0';
-    setTimeout(function() {
-      loader.remove();
-    }, 1000);
-  }, 1500);
-});
